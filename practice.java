1663c136fff928fd8c2379fec0b75c455264e8e9
ankitraj048 <ankit.raj@dianapps.com>
2025-09-02 11:17:49 +0530

    commit for new branch

--- practice.java
+++ practice.java
@@ -5,5 +5,7 @@ class practice{
     public static  void main(String []args){
         System.out.println("I am Ankit Pandey");
         System.out.println("Modifying the java file");
+        System.out.println("modified");
+        System.out.println("I am the commit for the otp-generator branch only ,  as you have switched yur branch from master to otp-generator, so u cant see this changes made in your master branch");
     }
 }
