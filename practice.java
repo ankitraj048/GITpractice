c34024d5f23450bda9ca85155e4beefb1c933b01
ankitraj048 <ankit.raj@dianapps.com>
2025-09-02 11:32:21 +0530

    my commit

--- practice.java
+++ practice.java
@@ -7,5 +7,9 @@ class practice{
         System.out.println("Modifying the java file");
         System.out.println("modified");
         System.out.println("I am the commit for the otp-generator branch only ,  as you have switched yur branch from master to otp-generator, so u cant see this changes made in your master branch");
+        int i=0;
+        if(i==0){
+            System.out.print("you are failure");
+        }
     }
 }
